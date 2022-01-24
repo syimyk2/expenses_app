e14edc7f00b9e7e2337eeefae325073746500d8f
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-01-24 12:35:28 +0600

    option-all

--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -5,9 +5,22 @@ import ExpenseItem from './ExpenseItem'
 const ExpensesList = (props) => {
     let expensesContent = <h2>NO Expenses found</h2>// озгормону  н2 тегке барабарлап койдук
 
-
+  if (props.filteredYear ==="all") ( // если пропстан келген филтередеар олл го барабар болсо 
+      
+     expensesContent =  props.total.map((element) => { // жогоорудагы пременныйга пропстан келген массивти аралап експенссайтемге  керектуу пропстарын бергенин барабарлап койдук
+          return ( // бул условия бизге баардык карточкалады чыгарып берет
+            <ExpenseItem
+              key={Math.random()}
+              title={element.title}
+              amount={element.amount}
+              date={element.date}
+            />
+          );
+        })
+      
+  )
   if (props.expenses.length>0) { //если длина филтрованного массива который мы получаем через пропс будет больше 0
-    expensesContent = props.expenses.map((element) => { //  то выше обявленную переменую мы приссваиваем ниже написанную операцию мар
+    expensesContent = props.expenses.map((element) => { //  то выше обявленную переменую м приссваиваем ниже написанную операцию мар
         return (
           <ExpenseItem
             key={Math.random()} // уникалный ключ катары рандомный сан берип койдук реакт туура иштеши учун
